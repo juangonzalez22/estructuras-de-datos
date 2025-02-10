2b70c69276700deac389959f511f6f7d1a5dd9bb
Juanjo <juan.gonzalez22@utp.edu.co>
2025-02-10 10:22:18 -0500

    Update

--- Punteros/punteros.cpp
+++ Punteros/punteros.cpp
@@ -6,6 +6,9 @@ int main(){
 
     int a = 10; // a es un entero, con valor 10, 
     int *puntero; // puntero es un puntero a un entero, pero no tiene valor asignado
+    int *punteroAPuntero; // punteroAPuntero es un puntero a un puntero a un entero, pero no tiene valor asignado
+
+    //Un puntero es una variable que almacena la direccion de memoria de otra variable
 
     puntero = &a; // puntero ahora apunta a la direccion de memoria de a
 
@@ -13,7 +16,10 @@ int main(){
     cout << "Direccion de a: " << &a << endl;
     cout << "Valor de puntero: " << puntero << endl;
     cout << "Direccion de puntero: " << &puntero << endl;
+    cout << "Valor de punteroAPuntero: " << punteroAPuntero << endl;
+    cout << "Direccion de punteroAPuntero: " << &punteroAPuntero << endl;
     cout << "Valor de a traves del puntero: " << *puntero << endl;
+
 
     return 0;
 }
